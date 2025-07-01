aad62a4d4afc94f811d035a6a0c92974d97be05f
Hamza <hamzakhan174722@gmail.com>
2025-07-01 23:11:52 +0500

    feat: implement Lottery contract with ticket purchasing and winner selection functionality

--- hardhat-lottery/smart-contracts/contract/lottery.sol
+++ hardhat-lottery/smart-contracts/contract/lottery.sol
@@ -10,25 +10,30 @@ pragma solidity ^0.8.28;
 error Lottery__NotEnoughETHForEntranceFee();
 
 contract Lottery {
+    address payable[] private s_players; // when we find winner then we pay them so it is payable
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function buyTicket() {
+    function buyTicket() public payable {
         // msg.value < i_entranceFee | if sender have not enough eth so it cause error message
         if (msg.value < i_entranceFee) {
             revert Lottery__NotEnoughETHForEntranceFee();
-        } else {}
+        } else {
+            s_players.push(payable(msg.sender));
+        }
     }
     function pickRandomWinner() public {}
-    function pickRandomWinner() {}
 
     // -------------------
     //  Getters
     // -------------------
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+    function getPalyers(uint256 index) public view returns (address) {
+        return s_players[index];
     }
 }
