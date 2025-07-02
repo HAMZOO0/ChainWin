4cc9d236ffed6e18859229f9bdaf17be86d65666
Hamza <hamzakhan174722@gmail.com>
2025-07-02 14:45:49 +0500

    fix: correct function name typo and enhance event emission in buyTicket

--- hardhat-lottery/smart-contracts/contract/lottery.sol
+++ hardhat-lottery/smart-contracts/contract/lottery.sol
@@ -13,16 +13,23 @@ contract Lottery {
     address payable[] private s_players; // when we find winner then we pay them so it is payable
     uint256 private immutable i_entranceFee;
 
+
+    /* Events */
+    event TicketBought ( address indexed player){}
+
+    /* constructor */
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
+    /* buyTicket */
     function buyTicket() public payable {
         // msg.value < i_entranceFee | if sender have not enough eth so it cause error message
         if (msg.value < i_entranceFee) {
             revert Lottery__NotEnoughETHForEntranceFee();
         } else {
             s_players.push(payable(msg.sender));
+            emit TicketBought (msg.sender);
         }
     }
     function pickRandomWinner() public {}
@@ -33,7 +40,7 @@ contract Lottery {
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
-    function getPalyers(uint256 index) public view returns (address) {
+    function getPlayers(uint256 index) public view returns (address) {
         return s_players[index];
     }
 }
