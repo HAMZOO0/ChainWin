7dfb10a15a1cc0ee2241d3ceca6642613c7c9d7e
Hamza <hamzakhan174722@gmail.com>
2025-07-05 22:45:36 +0500

    fix: Correct indentation in network configuration for consistency

--- hardhat-lottery/smart-contracts/helper-hardhat-config.js
+++ hardhat-lottery/smart-contracts/helper-hardhat-config.js
@@ -22,7 +22,7 @@ const networkConfig = {
       subscriptionId: "",
       keyHash:
          "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae",
-   },
+    },
 };
 
 const developmentChains = ["hardhat", "localhost"];
