39046a66191289580391d319099e2250c3c97a13
Hamza <hamzakhan174722@gmail.com>
2025-07-22 16:52:53 +0500

    feat: add ABI and contract address JSON files for frontend integration

--- hardhat-lottery/smart-contracts/deploy/03-updateFrontEnd.js
+++ hardhat-lottery/smart-contracts/deploy/03-updateFrontEnd.js
@@ -1,4 +1,9 @@
+const { deployments, getNamedAccounts, ethers } = require("hardhat");
+const fs = require("fs");
 module.export = async () => {
+   const FRONT_END_ADDRESS_FILE = "../../frontend/src/constants/contractAddress.json";
+   const FRONT_END_ABI_FILE = "../../frontend/src/constants/abi.json";
+
    // if we are updating frontend
    if (process.env.UPDATING_FRONT_END) {
       console.log(" Updating Frontend Contract");
@@ -6,4 +11,11 @@ module.export = async () => {
    }
 };
 
-async function UpdatingContractAddresses() {}
+async function UpdatingContractAddresses() {
+   // get the address of contract
+   const vrfCoordinatorDeployment = await deployments.get("Lottery");
+
+   // get older address
+   const oldContractAddress = JSON.parse(fs.readFileSync(FRONT_END_ADDRESS_FILE));
+   const oldAbi = JSON.parse(fs.readFileSync(FRONT_END_ABI_FILE, "utf-8"));
+}
